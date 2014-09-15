5312c324c9eff8822c83d293929cfe6fd299ee65
Yitian Chen <jasonyt@Chens-MacBook-Pro.local>
2014-09-15 11:47:55 -0400

    auto push

---
+++ eecs485_p1_git.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# This is the script for eecs485 project 1.
+# It push updates from local machine to Github
+# and then pull from the CAEN 485 machine.
+
+# Version 1
+# TODO: add commit message input
+
+# PUSH PROCESS
+cd ~/Desktop/Git_script_test/
+git pull
+git commit -a -m "auto push"
+git push
+
+# PULL PROCESS
+ssh jasonyt@login-course.engin.umich.edu
+cd Desktop/Git_script_test/Git_script_test/
+git pull
+exit
+
